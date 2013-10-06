6cfbe75fdaf01f556944110771782635963fbdbc
slynchot <lynch.s@husky.neu.edu>
2013-10-06 19:53:12 -0400

    huge initial commit- adding all project files so far

---
+++ StenographyQuest/Assets/Game Scripts/bullet.js
@@ -0,0 +1,63 @@
+﻿#pragma strict
+
+
+var someObject; 
+
+var directionator1 : Directionator;
+
+var shooting = false;
+
+var s1 = "textt";
+
+function Start () {
+
+}
+
+
+function Update () {
+
+if(shooting){
+
+transform.localPosition.z += 2;
+
+
+}
+
+
+
+/*
+else{
+directionator1 = GetComponent(Directionator);
+transform.position = directionator1.transform.position;
+}
+*/
+
+if (Input.GetKeyDown(KeyCode.F)){
+shooting = true;
+
+}
+}
+
+
+
+function OnTriggerEnter(hit : Collider)
+
+{
+if(hit.gameObject.name  ==  "Wall1")          // tagging the brick wall makes it easier to refer to in code
+{
+    s1 = "hit Wall_1"; }
+ 
+if(hit.gameObject.name  ==  "Wall2")          // tagging the brick wall makes it easier to refer to in code
+{
+    s1 = "hit Wall_2"; }
+ 
+if(hit.gameObject.name  ==  "Wall0")          // tagging the brick wall makes it easier to refer to in code
+{
+    s1 = "hit Wall_0"; }
+    
+}
+
+function OnGUI() {
+GUI.color = Color.red;
+	 GUI.Box(Rect(460,120,150,400),"info: "+s1);
+}
---
+++ StenographyQuest/Assets/Game Scripts/room.js
@@ -0,0 +1,60 @@
+﻿#pragma strict
+
+var roomnumber = 1;
+
+var roomtext = "";
+
+var OD = "";
+function Start () {
+
+}
+
+function Update () {
+
+outtext(roomnumber,OD);
+
+
+
+}
+
+public function upod(st){
+
+OD = st;
+
+}
+
+
+public function outtext(rn,dir){
+	if(room == "room1"){
+		if(dir == "North"){
+		roomtext = "this is the north of the room";
+		}
+		if(dir == "Northeast"){
+		roomtext = "this is the northeast of the room";
+		}
+		if(dir == "East"){
+		roomtext = "this is the east of the room";
+		}
+		if(dir == "Southeast"){
+		roomtext = "this is the southest of the room";
+		}
+		if(dir == "South"){
+		roomtext = "this is the south of the room";
+		}
+		if(dir == "Southwest"){
+		roomtext = "this is the southwest of the room";
+		}
+		if(dir == "West"){
+		roomtext = "this is the west of the room";
+		}
+		if(dir == "Northwest"){
+		roomtext = "this is the northwest of the room";
+		}
+		else{roomtext = "this is any direction";}
+	}
+	else{roomtext = "You're in a room";}
+}
+
+function OnGUI() {
+	 GUI.Box(Rect(5,5,430,400),roomtext);
+}
